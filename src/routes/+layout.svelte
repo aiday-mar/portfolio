c13bcbb8065188171798be87cc2f4484acfd9868
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-03-30 23:00:12 +0200

    fixing the scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import "../app.css";
+    import { beforeNavigate } from "$app/navigation";
     import Navbar from "$lib/components/Navbar.svelte";
     import Footer from "$lib/components/Footer.svelte";
 
     let { children } = $props();
+
+    beforeNavigate(() => {
+        document.documentElement.style.scrollBehavior = "auto";
+        requestAnimationFrame(() => {
+            document.documentElement.style.scrollBehavior = "";
+        });
+    });
 </script>
 
 <svelte:head>
